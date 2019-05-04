ee8ba16dc1dd5c629eacc971da6678c8c36bf9bc
Daniel_GL <luhniel1@gmail.com>
2019-05-03 21:34:03 -0300

    Arquivo principal do sitema

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<title>Home | CrontrolFip</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h1>ControlFip</h1>
+		<h2>Sistema de Controle Financeiro Pessoal</h2>
+		<?php 
+		
+		 ?>
+	</body>
+</html>
