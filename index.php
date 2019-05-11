a8b2299cb4c9ab07a4d5a7ec00f63124d7ddcad2
Daniel_GL <luhniel1@gmail.com>
2019-05-10 21:28:59 -0300

    Link Contas a Receber

--- index.php
+++ index.php
@@ -3,12 +3,19 @@
 	<head>
 		<title>Home | CrontrolFip</title>
 		<meta charset="utf-8">
+		<style type="text/css">
+			body{
+				background-color: #000;
+				color: #fff;
+			}
+		</style>
 	</head>
 	<body>
 		<h1>ControlFip</h1>
 		<h2>Sistema de Controle Financeiro Pessoal</h2>
 		<div>
 			<a href="contas_apagar.php">Contas a Pagar</a>
+			<a href="contas_receber.php">Contas a Receber</a>
 		</div>
 		<?php 
 		
