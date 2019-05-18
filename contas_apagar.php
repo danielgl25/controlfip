0cfa79b502d79d21528f3032bd43c59984547585
Daniel_GL <luhniel1@gmail.com>
2019-05-17 21:26:51 -0300

    Criando Contas a Pagar

---
+++ contas_apagar.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<title>Controlfip | Contas a Pagar</title>
+		<meta charset="utf-8">
+		<style type="text/css">
+			body{
+				background-color: #000;
+				color: #fff;
+			}
+			a{
+				padding: 5px;
+				color: #fff;
+				text-decoration: none;
+				border-bottom: 2px solid #870000;
+				border-radius: 10px;
+				transition: .5s;
+			}
+			a:hover{
+				background-color: #870000;
+			}
+			h1,h2{
+				text-align: center;
+			}
+		</style>
+	</head>
+	<body>
+		<h1>Contas a Pagar</h1>
+	</body>
+</html>
